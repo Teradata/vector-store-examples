70e3a6988b0a430b35569fb29a7320c77cb202c8
Jessica Velez <jessica.velez@teradata.com>
2025-11-03 11:31:21 -0600

    Create settings.py

---
+++ In_DB_Analytics_Functions/UseCase11/settings.py
@@ -0,0 +1,5 @@
+#settings.py : Environment settings for usecaseNN_Vantage_python.py scripts
+system_dsn = "vantage24.td.teradata.com"
+system_db = "TPCXAI"
+system_user = "XXXX"
+system_pw = "XXXXX"
